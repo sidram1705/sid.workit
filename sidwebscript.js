24b489f68f9cf96687eb2a7df29c161a8a852f84
Siddharth Ramanathan <72404506+sidram1705@users.noreply.github.com>
2020-10-11 23:32:36 +0530

    Create sidwebscript.js

---
+++ sidwebscript.js
@@ -0,0 +1,22 @@
+// Wait till the DOM is ready.
+$(function () {
+  var roles = ["Speed cuber", "Student", "VITian","Drummer","Foodie"];
+  var count = 0;
+  var $typeSpan = $("#intro .content h2 span").typist({
+    text: roles[count]
+  });
+  setInterval(() => {
+    $typeSpan
+      .typistRemove(roles[count++ % roles.length].length)
+      .typistPause(2000)
+      .typistAdd(roles[count % roles.length]);
+  }, 5000);
+  $(".navbar").hide();
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 40) {
+      $(".navbar").slideDown(100);
+    } else {
+      $(".navbar").slideUp(100);
+    }
+  });
+});
